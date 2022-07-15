0fa7e63a0e5f5e6651e79e9baa400bb058f246db
Eddie Joseph <eddie.joseph@windowslive.com>
2022-07-15 19:26:28 +0200

    added basic sanity checks

--- read_file.py
+++ read_file.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pandas as pd
 
 def read_groups(groups_path:str='Einheiten.xlsx'):
@@ -6,6 +8,11 @@ def read_groups(groups_path:str='Einheiten.xlsx'):
 
 def read_orders(orders_path:str):
     df = pd.read_excel(orders_path, sheet_name=0)
+
+    if df[(df['MenuPlanDate'] < datetime.strptime('23.7.22', '%d.%m.%y'))  | (df['MenuPlanDate'] > datetime.strptime('6.8.22', '%d.%m.%y'))].shape[0] > 0:
+        raise Exception("Orders Date out of range")
+    if df[(df['MenuPlanDate']=='')].shape[0] > 0:
+        raise Exception("Orders Date not set")
     return df
 
 def read_prices(prices_path:str):
@@ -14,4 +21,8 @@ def read_prices(prices_path:str):
 
 def read_classification(classification_path:str):
     df = pd.read_excel(classification_path, sheet_name=0)
+    if df[(df['Kategorie numerisch']!=1)&(df['Kategorie numerisch']!=2)&(df['Kategorie numerisch']!=3)].shape[0] > 0:
+        raise Exception("Classification Kategorie not set")
+    if df[(df['Sortierung']=='')].shape[0] > 0:
+        raise Exception("Classification Sortierung not set")
     return df
